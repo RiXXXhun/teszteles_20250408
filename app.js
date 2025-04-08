d294786aa02ff581ec7b5a0f87d386dfc82f041c
RiXXXhun <100133611+RiXXXhun@users.noreply.github.com>
2025-04-08 17:02:58 +0200

    aa

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ function osszefuz(a, b) {
     return `${a} - ${b}`
 }
 
-/*
+
 function arrayContains(tomb, elem) {
     return tomb.includes(elem)
 }
-*/
+
 
 function getGrade(limit, point) {
     const percent = point / limit * 100;
@@ -28,9 +28,43 @@ function getGrade(limit, point) {
     return 1;
 }
 
+function addXTimes(base, add, times) {
+    let value = base;
+    for (let i = 0; i< times; i++) {
+        value += add;
+    }
+    return value;
+}
+
+function addArrayElement(tomb, elem) {
+    if(elem !== null) {
+        tomb.push(elem)
+    }
+    
+    return tomb;
+}
+
+function convertItemsToString(item) {
+    return `${item}`;
+}
+
+function mapArrayElements(tomb) {
+    return tomb.map(item => convertItemsToString(item))
+}
+
+function getRandomByChance(chance = 50) {
+    return Math.random() < (chance / 100)
+
+}
 
 module.exports = {
     sum,
     osszefuz,
-    getGrade
+    getGrade,
+    addXTimes,
+    arrayContains, 
+    addArrayElement,
+    convertItemsToString,
+    mapArrayElements,
+    getRandomByChance
 };
--- app.test.js
+++ app.test.js
@@ -1,7 +1,9 @@
-const {sum, osszefuz, getGrade} = require("./app")
+const {sum, osszefuz, getGrade, addXTimes, arrayContains, addArrayElement, mapArrayElements, convertItemsToString, getRandomByChance} = require("./app")
 
 test("add 4 + 5 to equal 9", () => {
     expect(sum(4, 5)).toBe(9)
+    expect(sum(3, 3)).toBeGreaterThan(3)
+    expect(sum(3, 3)).toBeLessThan(10)
 })
 
 test("concat apple and orange we get 'apple - orange' ", () => {
@@ -21,3 +23,44 @@ test("get specific grade", () => {
     expect(getGrade(100, 25)).toBe(2)
     expect(getGrade(100, 55)).toBe(3)
 })
+
+test("adding 2 , 5 times to 10 eqals 20", () => {
+    expect(addXTimes(10, 2, 5)).toBe(20)
+})
+
+test("array caontains apple ", () => {
+    const tomb = [
+        'apple',
+        'banana',
+        'orange'
+    ]
+
+    expect(arrayContains(tomb, 'apple')).toBeTruthy()
+    expect(arrayContains(tomb, 'egg')).toBeFalsy()
+})
+
+test("array lemght will be 1 longer aster add", () => {
+    const tomb = [1, 150, 2];
+    
+    expect(addArrayElement([...tomb], 99)).toHaveLength(tomb.length + 1)
+})
+
+test("adding null will be not inscre array lenght", () => {
+    const tomb = [1, 150, 2];
+
+    expect(addArrayElement([...tomb], null)).toHaveLength(tomb.length)
+})
+
+test("mapped array first elemnt is string", () => {
+    const tomb = [1, 2, 3]
+    const mapped =  mapArrayElements(tomb);
+
+    expect(mapped[0]).toStrictEqual('1')
+})
+
+test("get truo or false", () => {
+    expect(getRandomByChance(0)).toBeFalsy()
+    expect(getRandomByChance(100)).toBeTruthy()
+    expect(typeof getRandomByChance()).toBe("boolean")
+})
+
